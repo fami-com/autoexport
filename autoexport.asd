424d68ae5fb801048fa79ef04cf95b184472692e
fam <me@succ.dk>
2021-08-10 22:56:33 +0300

    Initial commit

---
+++ autoexport.asd
@@ -0,0 +1,10 @@
+(defpackage :autoexport-system
+  (:use :cl :asdf))
+
+(in-package :autoexport-system)
+
+(defsystem :autoexport
+    :serial t
+    :depends-on (:closer-mop :alexandria)
+    :components
+    ((:file "pub")))
---
+++ autoexport.lisp
@@ -0,0 +1,30 @@
+(defpackage :autoexport
+  (:use :cl)
+  (:export :pub)
+  (:local-nicknames (:ax :alexandria)))
+
+(in-package :autoexport)
+
+(defmacro pub (what name &body body)
+  (eval-when (:compile-toplevel
+              :load-toplevel
+              :execute)
+    (ax:with-gensyms (class slot slot-name readers reader writers writer)
+      `(progn
+         (,what ,name ,@body)
+         ,@(when (eq what 'defclass)
+            `((let ((,class (find-class ',name)))
+                (c2mop:ensure-finalized ,class)
+                (loop for ,slot in (c2mop:class-direct-slots ,class)
+                   for ,slot-name = (c2mop:slot-definition-name ,slot)
+                   for ,readers = (c2mop:slot-definition-readers ,slot)
+                   for ,writers = (c2mop:slot-definition-writers ,slot)
+                   do (export ,slot-name)
+                   do (loop for ,reader in ,readers do
+                           (export ,reader))
+                   do (loop for ,writer in ,writers
+                         if (listp ,writer) do
+                           (export (cadr ,writer))
+                         else do
+                           (export ,writer))))))
+         (export ',name)))))
